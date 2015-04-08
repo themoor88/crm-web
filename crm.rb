3506413ac3a7635fa4ce7181f2cf2c0518a8412a
Wajid Kagzi <wjkagzi@gmail.com>
2015-04-08 18:00:37 -0400

    Finished the first part of the CRM web application.

---
+++ crm.rb
@@ -0,0 +1,44 @@
+require_relative 'rolodex'
+require_relative 'contact'
+require 'sinatra'
+
+$rolodex = Rolodex.new
+# A route we made. For this particular URL and GET method this is the response.
+get '/' do
+  # puts "Hello World"
+  # puts params
+  @crm_app_name = "My CRM"
+  erb :index
+  # Sinatra convention is to store erb files in views.
+end
+
+# Things that show up in the terminal is called the terminal log.
+# Params hash collects information and is stored.
+
+get '/contacts' do
+  erb :contacts
+end
+
+get '/contacts/new' do
+  erb :new_contact
+end
+
+post '/contacts' do
+  new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
+  $rolodex.store_new_contact(new_contact)
+  redirect to('/contacts')
+end
+
+# get '/:name' do
+#   @name = params[:name].capitalize
+  # Instance variable so it can be used outside of the route.
+  # Captures the key value from the params hash.
+  # Can put symbol instead of string.
+  # :name is a wildcard and params will store it in that hash.
+  # "Hello #{name.capitalize}"
+#   erb :hello
+# end
+
+# Mornings are only good at the slash level.
+# Params hash disappears after it has been used.
+# erb means embedded ruby. It gets read first.
---
+++ rolodex.rb
@@ -0,0 +1,67 @@
+class Rolodex
+  attr_reader :contacts
+
+  def self.attr_reader(name)
+      def #{name}
+        @#{name}
+      end
+  end
+
+  def contacts=(value)
+    @contacts = value
+  end
+
+  def initialize
+    @contacts = []
+    @id = 1
+  end
+
+  def find(contact_id)
+    @contacts.find do |contact|
+      contact.id == contact_id
+    end
+  end
+
+  # def search(query)
+  #   if query == "first name"
+  #   @contacts.select do |contact|
+  #     contact.first_name == first_name
+  #   elsif query == "first name"
+  #   @contacts.select do |contact|
+  #     contact.first_name == first_name
+  #   elsif query == "first name"
+  #   @contacts.select do |contact|
+  #     contact.first_name == first_name
+  #   elsif query == "first name"
+  #   @contacts.select do |contact|
+  #     contact.first_name == first_name
+  #   end
+  # end
+
+  def store_new_contact(contact)
+    contact.id = @id
+    @contacts << contact
+    @id += 1
+  end
+
+  def modify_contact(contact, attribute, new_attribute)
+      if attribute == "first name"
+        contact.first_name = new_attribute
+      elsif attribute == "last name"
+        contact.last_name = new_attribute
+      elsif attribute == "email"
+        contact.email = new_attribute
+      elsif attribute == "notes"
+        contact.notes = new_attribute
+      else
+        puts "That is an invalid attribute."
+      end
+  end
+  # Question: WHy can't I run contact.attribute?
+
+  def delete_contact(contact_id)
+    @contacts.delete_if do |contact|
+    contact.id == contact_id
+    end
+  end
+end
